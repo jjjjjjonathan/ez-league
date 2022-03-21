58e903d58ad7ccfd28b9f980c5b27a7f2873f4b0
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-20 21:23:30 -0400

    Add route to get teams in league

--- db/seeds/02_teams.sql
+++ db/seeds/02_teams.sql
@@ -6,4 +6,5 @@ VALUES ('Chelsea', 1, 65, 46, 18, 10, 10, 'https://resources.premierleague.com/p
 ('Tottenham Hotspur', 1, 66, 41, 20, 9, 9, 'https://resources.premierleague.com/premierleague/badges/25/t6.png'),
 ('Newcastle United', 1, 56, 51, 19, 8, 11, 'https://resources.premierleague.com/premierleague/badges/25/t4.png'),
 ('Everton', 1, 50, 40, 15, 11, 12, 'https://resources.premierleague.com/premierleague/badges/25/t11.png'),
-('Liverpool', 1, 47, 40, 14, 10, 14, 'https://resources.premierleague.com/premierleague/badges/25/t14.png');
+('Liverpool', 1, 47, 40, 14, 10, 14, 'https://resources.premierleague.com/premierleague/badges/25/t14.png'),
+('Fulham', 1, 48, 51, 14, 10, 14, 'https://resources.premierleague.com/premierleague/badges/25/t54.png');
--- routes/leagues.js
+++ routes/leagues.js
@@ -2,12 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = db => {
+  // Select all the leagues, returns id, name, year, logo url, and cover photo url... http://localhost:8001/api/leagues
   router.get('/', (req, res) => {
-    // Select all the leagues, returns id, name, year, logo url, and cover photo url... http://localhost:8001/api/leagues
     return db.query('SELECT * FROM leagues;')
       .then(data => {
         res.json(data.rows);
       });
   });
+
+  // Get all the teams from one league in order of points descending, then goal difference descending... http://localhost:8001/api/leagues/:id
+
+  router.get('/:id', (req, res) => {
+    return db.query(`SELECT teams.*,
+    teams.wins * 3 + teams.draws AS points,
+    teams.goals_for - teams.goals_against AS goal_difference
+    FROM teams
+    WHERE league_id = $1
+    ORDER BY points DESC, goal_difference DESC;`, [req.params.id])
+      .then(data => {
+        res.json(data.rows);
+      });
+  });
+
   return router;
 };
