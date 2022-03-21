23f09cbcae689572e3ed3ca53d062678c758b4c6
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-21 17:53:45 -0400

    Fix state and table list

--- src/App.js
+++ src/App.js
@@ -2,55 +2,23 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import TableList from "./component/TableList";
-import TableListItem from "./component/TableListItem";
-
-const team = [
-  {
-    id: 2,
-    league_id: 1,
-    name: "Manchester City",
-    thumbnail_logo:
-      "https://resources.premierleague.com/premierleague/badges/25/t43.png",
-    goals_for: 93,
-    goals_against: 29,
-    wins: 28,
-    draws: 5,
-    losses: 5,
-    points: 89,
-    goal_difference: 64,
-  },
-  {
-    id: 3,
-    league_id: 1,
-    name: "Manchester United",
-    thumbnail_logo:
-      "https://resources.premierleague.com/premierleague/badges/25/t1.png",
-    goals_for: 89,
-    goals_against: 33,
-    wins: 28,
-    draws: 5,
-    losses: 5,
-    points: 89,
-    goal_difference: 56,
-  },
-];
 
 function App() {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    leagues: [],
+    teams: []
+  });
 
   useEffect(() => {
     Promise.all([axios.get("/api/leagues"), axios.get("/api/leagues/1")]).then(
       (all) => {
         const [leagues, teams] = all;
-        setState((prev) => ({ ...prev, leagues, teams }));
+        setState((prev) => ({ ...prev, leagues: leagues.data, teams: teams.data }));
       }
     );
   }, []);
 
-  // console.log("this is state", state.teams);
-  // console.log("this is state JSON parse", JSON.parse(state.teams.data));
-  return <TableList teams={team} />;
-  // return <h1>Hello</h1>;
+  return <TableList teams={state.teams} />;
 }
 
 export default App;
--- src/component/TableList.js
+++ src/component/TableList.js
@@ -1,25 +1,26 @@
-import { Fragment } from "react";
 import TableListItem from "./TableListItem";
 
 const TableList = (props) => {
   const { teams } = props;
   const teamList = teams.map((team, index) => (
-    <TableListItem key={team.id} rank={index} {...team} />
+    <TableListItem key={team.id} rank={index + 1} {...team} />
   ));
 
   return (
     <table>
-      <tr>
-        <th>Club</th>
-        <th scope="col">MP</th>
-        <th scope="col">W</th>
-        <th scope="col">D</th>
-        <th scope="col">L</th>
-        <th scope="col">GF</th>
-        <th scope="col">GA</th>
-        <th scope="col">GD</th>
-        <th scope="col">pts</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Club</th>
+          <th scope="col">MP</th>
+          <th scope="col">W</th>
+          <th scope="col">D</th>
+          <th scope="col">L</th>
+          <th scope="col">GF</th>
+          <th scope="col">GA</th>
+          <th scope="col">GD</th>
+          <th scope="col">pts</th>
+        </tr>
+      </thead>
       <tbody>{teamList}</tbody>
     </table>
   );
