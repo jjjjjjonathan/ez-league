bcde14d71a49d4797b7281420fd6cef4ed9c7621
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-20 21:06:49 -0400

    Add teams schema and seeds

--- routes/leagues.js
+++ routes/leagues.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = db => {
-  router.get('/', async = (req, res) => {
-    // Select all the leagues, returns id, name, year, logo url, and cover photo url
+  router.get('/', (req, res) => {
+    // Select all the leagues, returns id, name, year, logo url, and cover photo url... http://localhost:8001/api/leagues
     return db.query('SELECT * FROM leagues;')
       .then(data => {
         res.json(data.rows);
       });
-
   });
   return router;
 };
