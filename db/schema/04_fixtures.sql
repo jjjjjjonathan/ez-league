82e387d3b52d1c8e910e6b3a69ba39c07685f77f
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-21 15:59:06 -0400

    Add fixture events schema and seeds

--- db/schema/04_fixtures.sql
+++ db/schema/04_fixtures.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS fixtures CASCADE;
 CREATE TABLE fixtures (
   id SERIAL PRIMARY KEY NOT NULL,
-  league_id INTEGER REFERENCES leagues(id) ON DELETE CASCADE,
-  home_team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE,
-  away_team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE,
+  league_id INTEGER REFERENCES leagues(id) ON DELETE CASCADE NOT NULL,
+  home_team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE NOT NULL,
+  away_team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE NOT NULL,
   status VARCHAR(255) DEFAULT 'Upcoming',
   scheduled_time TIMESTAMP,
   first_half_start_time TIMESTAMP,
