5bba4e5b9c6bd12a53cce87d071061c077137bbc
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-22 10:17:39 -0400

    Ensure react-router-dom works

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
 import useApplicationData from './hooks/useApplicationData';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import TestRoute from './components/TestRoute';
 
 function App() {
-
   const { state } = useApplicationData();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing state in ez-league
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/routes">React Router Testing</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route path="/routes">
+          <TestRoute />
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Testing state in ez-league</p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
