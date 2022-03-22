4cf48e177c4584060174d18358b55bed7632d651
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-22 09:40:05 -0400

    Add routes to all teams in app

--- routes/teams.js
+++ routes/teams.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = db => {
-  // Select players from team of given id
+  // SELECT ALL TEAMS TO PUT INTO STATE
+  router.get('/', (req, res) => {
+    return db.query('SELECT * FROM teams;')
+      .then(data => {
+        res.json(data.rows);
+      });
+  });
+
+  // Select players from team of given id NEEDS TO BE DELETED
   router.get('/:id', (req, res) => {
     return db.query(`SELECT players.*
     FROM players
