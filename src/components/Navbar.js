a88892c642fd41ca770193743f0692546a6d4b3d
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-03-22 17:36:36 -0400

    add some layout in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Link to="/">Home&nbsp;&nbsp;</Link>
-
-        <Link to="/standing">League Table</Link>
-      </div>
+    <nav className="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
+      <ul className="flex items-center cursor-pointer md:flex md:items-center">
+        <li className="md:mx-4 hover:text-gray-400">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="md:mx-4 hover:text-gray-400">
+          <Link to="/standing">League Table</Link>
+        </li>
+        <li className="md:mx-4 hover:text-gray-400">
+          <Link to="/teamform">Team Form</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
