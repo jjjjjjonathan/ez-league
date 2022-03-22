69363f9ec426829acf4204541e811f985fa56249
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-22 16:06:27 -0400

    Add new axios calls for state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,14 +12,25 @@ const useApplicationData = () => {
 
   useEffect(() => {
     Promise.all([
+      axios.get('/api/sports'),
       axios.get('/api/leagues'),
       axios.get('/api/teams'),
       axios.get('/api/players'),
       axios.get('/api/fixtures'),
-      axios.get('/api/fixtures/events')
+      axios.get('/api/fixtures/events'),
+      axios.get('/api/fixtures/types')
     ]).then((all) => {
-      const [leagues, teams, players, fixtures, fixtureEvents] = all;
-      setState(prev => ({ ...prev, leagues: leagues.data, teams: teams.data, players: players.data, fixtures: fixtures.data, fixtureEvents: fixtureEvents.data }));
+      const [sports, leagues, teams, players, fixtures, fixtureEvents, fixtureTypes] = all;
+      setState(prev => ({
+        ...prev,
+        sports: sports.data,
+        leagues: leagues.data,
+        teams: teams.data,
+        players: players.data,
+        fixtures: fixtures.data,
+        fixtureEvents: fixtureEvents.data,
+        fixtureTypes: fixtureTypes.data
+      }));
     });
   }, []);
 
