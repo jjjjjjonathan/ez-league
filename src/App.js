d51c53d73d2e909b7457d53fb4d7bf12a6d0d328
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-03-22 19:42:00 -0400

    add layout for team form

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ function App() {
   const { state } = useApplicationData();
   console.log(state);
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/routes">
-          <TestRoute />
-        </Route>
-        <Route exact path="/standing">
-          <Standing teams={state.teams} />
-        </Route>
-        <Route exact path="/teamform">
-          <TeamForm />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="h-screen bg-gradient-to-b from-gray-50 to-gray-200">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/routes">
+            <TestRoute />
+          </Route>
+          <Route exact path="/standing">
+            <Standing teams={state.teams} />
+          </Route>
+          <Route exact path="/teamform">
+            <TeamForm />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
