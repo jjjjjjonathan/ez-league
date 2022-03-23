9f6c1fe3f5c47f88cf0270d7c733cd7d977751de
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-03-22 20:27:14 -0400

    add layout to csv-parsing

--- src/components/CSVReader.jsx
+++ src/components/CSVReader.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react';
-import classNames from 'classnames';
-import { parse } from 'papaparse';
+import { useState } from "react";
+import classNames from "classnames";
+import { parse } from "papaparse";
 
 const save = (list) => {
-  list.length <= 0 ? console.log('list is empty') : console.log(list);
+  list.length <= 0 ? console.log("list is empty") : console.log(list);
 };
 
 const CSVReader = () => {
   const [highlighted, setHighlighted] = useState(false);
   const [list, setList] = useState([]);
   const dropZoneClasses = classNames(
-    'p-6',
-    'my-2',
-    'mx-auto',
-    'max-w-md',
-    'border-2',
-    { 'border-green-600': highlighted },
-    { 'bg-green-100': highlighted }
+    "p-6",
+    "my-2",
+    "mx-auto",
+    "max-w-md",
+    "border-2",
+    { "border-green-600": highlighted },
+    { "bg-green-100": highlighted }
   );
 
   return (
-    <div>
-      <h1 className="text-center text-4xl">Contact Import</h1>
+    <div className="  mb-6 shadow-md bg-white rounded px-8 pt-6 pb-8 mb-4 m-20 ">
+      <h1 className="text-center text-4xl">Import Player</h1>
       <div
         className={classNames(dropZoneClasses)}
         onDragEnter={() => setHighlighted(true)}
@@ -35,8 +35,8 @@ const CSVReader = () => {
           Array.from(e.dataTransfer.files)
             .filter(
               (file) =>
-                file.type === 'application/vnd.ms-excel' ||
-                file.type === 'text/csv'
+                file.type === "application/vnd.ms-excel" ||
+                file.type === "text/csv"
             )
             .forEach(async (file) => {
               const text = await file.text();
@@ -46,7 +46,42 @@ const CSVReader = () => {
       >
         DROP HERE
       </div>
-      <button onClick={() => save(list)}>Submit</button>
+      <div className="md:flex md:justify-center mb-6">
+        <button
+          onClick={() => save(list)}
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded md:items-center"
+        >
+          Submit
+        </button>
+      </div>
+
+      {/* i found this component layout i think this is good if you can implementing with your send data */}
+
+      {/* <div class="max-w-xl">
+        <label class="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+          <span class="flex items-center space-x-2">
+            <svg
+              xmlns=""
+              class="w-6 h-6 text-gray-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+              />
+            </svg>
+            <span class="font-medium text-gray-600">
+              Drop files to Attach, or
+              <span class="text-blue-600 underline"> browse</span>
+            </span>
+          </span>
+          <input type="file" name="file_upload" class="hidden" />
+        </label>
+      </div> */}
     </div>
   );
 };
