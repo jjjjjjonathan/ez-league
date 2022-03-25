5c52727ccac377ce819377ad7e802cc4ac88d843
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-25 13:57:26 -0400

    comment out broken route

--- routes/fixtures.js
+++ routes/fixtures.js
@@ -10,32 +10,32 @@ module.exports = db => {
       });
   });
   // This route should be GET/fixtures/:id
-  router.get('/:id', (req, res) => {
+  // router.get('/:id', (req, res) => {
 
-    const fixtureId = req.params.id;
-    console.log("----", fixtureId)
-    const sqlquery = `
-      SELECT 
-      fixtures.home_team_id,
-      fixtures.home_team_score,
-      fixtures.away_team_id,
-      fixtures.away_team_score,
-      fixtures.scheduled_time, 
-      home_team.name AS home_team_name,
-      away_team.name AS away_team_name
-      FROM fixtures 
-      INNER JOIN teams AS home_team ON home_team.id = fixtures.home_team_id 
-      INNER JOIN teams AS away_team ON away_team.id = fixtures.away_team_id 
-      WHERE fixtures.id = $1
-      ORDER BY scheduled_time;`
-    const values = [fixtureId]
-    // :id = game 2 --> in game 2 ==> home vs away ==> man u v man c
-    return db.query(sqlquery, values)
-      .then(data => {
-        console.log("this is the db query results", data)
-        res.json(data.rows);
-      });
-  });
+  //   const fixtureId = req.params.id;
+  //   console.log("----", fixtureId)
+  //   const sqlquery = `
+  //     SELECT 
+  //     fixtures.home_team_id,
+  //     fixtures.home_team_score,
+  //     fixtures.away_team_id,
+  //     fixtures.away_team_score,
+  //     fixtures.scheduled_time, 
+  //     home_team.name AS home_team_name,
+  //     away_team.name AS away_team_name
+  //     FROM fixtures 
+  //     INNER JOIN teams AS home_team ON home_team.id = fixtures.home_team_id 
+  //     INNER JOIN teams AS away_team ON away_team.id = fixtures.away_team_id 
+  //     WHERE fixtures.id = $1
+  //     ORDER BY scheduled_time;`
+  //   const values = [fixtureId]
+  //   // :id = game 2 --> in game 2 ==> home vs away ==> man u v man c
+  //   return db.query(sqlquery, values)
+  //     .then(data => {
+  //       console.log("this is the db query results", data)
+  //       res.json(data.rows);
+  //     });
+  // });
   // Get all fixture events of all leagues to put into state
   router.get('/events', (req, res) => {
     return db.query('SELECT * FROM fixture_events;')
