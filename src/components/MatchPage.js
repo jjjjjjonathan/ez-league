ce03643c7f78d3f6613729bcde7f949afb984dec
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-26 15:58:18 -0400

    updates score, teams, and timer to pull data dynamically

--- src/components/MatchPage.js
+++ src/components/MatchPage.js
@@ -14,6 +14,12 @@ const MatchPage = (props) => {
     setTimerRef(seconds);
   };
 
+  const fixture = fixtures.find(fixture => fixture.id === parseInt(id))
+
+  const homeTeam = teams.find(team => team.id === fixture.home_team_id)
+
+  const awayTeam = teams.find(team => team.id === fixture.away_team_id)
+
   useEffect(() => {
     const matchId = 6; // FOR TEST PURPOSES WILL HAVE TO CHANGE DYNAMICALLY LATER 
     const route = `/api/fixtures/${matchId}`;
@@ -30,11 +36,82 @@ const MatchPage = (props) => {
     });
   }, []);
 
+
+
+  const [timerOn, setTimerOn] = useState(false);
+
+  const [time, setTime] = useState({
+    minutes: 0,
+    seconds: 0,
+  });
+
   useEffect(() => {
-    if (timer === 10) {
-      clearInterval(timerRef);
+    let timer1;
+    if (fixture.status === "First Half") {
+      setTimerOn(true);
+      timer1 = setInterval(() => {
+        let minutes = 0;
+        let seconds = Math.floor(
+          (Date.now() - Date.parse(fixture.first_half_start_time)) / 1000
+        );
+        if (seconds >= 60) {
+          minutes = Math.floor(seconds / 60);
+          seconds -= 60 * minutes;
+        }
+        setTime({
+          minutes,
+          seconds,
+        });
+        if (minutes === 45 || fixture.status === "Halftime") {
+          setTimerOn(false);
+        }
+      }, 1000);
     }
-  }, [timer]);
+    let timer2;
+
+    if (fixture.status === "Second Half") {
+      setTimerOn(true);
+      timer2 = setInterval(() => {
+        let minutes = 0;
+        let seconds =
+          Math.floor(
+            (Date.now() - Date.parse(fixture.second_half_start_time)) / 1000
+          ) + 2700;
+
+        if (seconds >= 60) {
+          minutes = Math.floor(seconds / 60);
+          seconds -= 60 * minutes;
+        }
+
+
+
+        setTime({
+          minutes,
+          seconds,
+        });
+
+        if (minutes === 90 || fixture.status === "Final") {
+          // clearInterval(timer1);
+          setTimerOn(false);
+        }
+      }, 1000);
+    }
+
+    if (!timerOn) {
+      clearInterval(timer1);
+      clearInterval(timer2);
+    }
+
+    return () => {
+      clearInterval(timer1);
+      clearInterval(timer2);
+    };
+  }, [
+    fixture.status,
+    fixture.first_half_start_time,
+    fixture.second_half_start_time,
+    timerOn,
+  ]);
 
   //fetching the data
   fetch("DYNAMIC URL GOES HERE", {
@@ -58,19 +135,24 @@ const MatchPage = (props) => {
         <h1>ScoreBoard</h1>
         <div className="title-box">
           <p>Local Team</p>
-          <p id="elapsed">{timer}</p>
-          <button onClick={startTimer}>Start Game</button>
+          <p id="elapsed">{time.minutes.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+          })}:{time.seconds.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+          })}</p>
           <p>Visitor Team</p>
         </div>
         <div className="title-box flex flex-row flex-no-wrap justify-around items-center w-full">
           <div clasName="team w-24">
             {/* <img  id="homeLogo" > */}
-            <p id="homeName">Team name</p>
+            <p id="homeName">{homeTeam.name}</p>
           </div>
-          <p id="goals">3  -  1</p>
+          <p id="Score">{fixture.home_team_score}-{fixture.away_team_score}</p>
           <div className="team">
             {/* <img id="awayLogo"> */}
-            <p id="awayName">Team name</p>
+            <p id="awayName">{awayTeam.name}</p>
           </div>
         </div>
         <div className="matchTable" class="matches-table mt-12 flex flex-col">
