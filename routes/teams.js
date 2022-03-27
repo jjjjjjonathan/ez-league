ed80b5091af726e2c0c04b93735f196b8d58ff17
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-03-27 14:20:10 -0400

    create route to update the teams stats

--- routes/teams.js
+++ routes/teams.js
@@ -1,32 +1,71 @@
 const express = require("express");
+const { restart } = require("nodemon");
 const router = express.Router();
 
-module.exports = db => {
+module.exports = (db) => {
   // SELECT ALL TEAMS TO PUT INTO STATE
-  router.get('/', (req, res) => {
-    return db.query('SELECT * FROM teams;')
-      .then(data => {
-        res.json(data.rows);
-      });
+  router.get("/", (req, res) => {
+    return db.query("SELECT * FROM teams;").then((data) => {
+      res.json(data.rows);
+    });
   });
 
   // Put create new teams route here
 
-  router.put('/', (req, res) => {
+  router.put("/", (req, res) => {
     const { queryString, queryParams } = req.body;
-    return db.query(queryString, queryParams)
-      .then(data => {
-        res.status(201).json(data.rows);
+    return db.query(queryString, queryParams).then((data) => {
+      res.status(201).json(data.rows);
+    });
+  });
+
+  router.put("/wins", (req, res) => {
+    const { wins, goalsFor, goalsAgainst, teamId } = req.body;
+    return db
+      .query(
+        `UPDATE teams SET wins = $1, goals_for = $2, goals_against= $3 WHERE id = $4 RETURNING *;`,
+        [wins, goalsFor, goalsAgainst, teamId]
+      )
+      .then((data) => {
+        res.status(200).json(data);
+      });
+  });
+
+  router.put("/losses", (req, res) => {
+    const { losses, goalsFor, goalsAgainst, teamId } = req.body;
+    return db
+      .query(
+        `UPDATE teams SET losses = $1, goals_for = $2, goals_against= $3 WHERE id = $4 RETURNING *;`,
+        [losses, goalsFor, goalsAgainst, teamId]
+      )
+      .then((data) => {
+        res.status(200).json(data);
+      });
+  });
+
+  router.put("/draws", (req, res) => {
+    const { draws, goalsFor, goalsAgainst, teamId } = req.body;
+    return db
+      .query(
+        `UPDATE teams SET draws = $1, goals_for = $2, goals_against= $3 WHERE id = $4 RETURNING *;`,
+        [draws, goalsFor, goalsAgainst, teamId]
+      )
+      .then((data) => {
+        res.status(200).json(data);
       });
   });
 
   // Select players from team of given id NEEDS TO BE DELETED
-  router.get('/:id', (req, res) => {
-    return db.query(`SELECT players.*
+  router.get("/:id", (req, res) => {
+    return db
+      .query(
+        `SELECT players.*
     FROM players
     WHERE team_id = $1
-    ORDER BY players.minutes DESC;`, [req.params.id])
-      .then(data => {
+    ORDER BY players.minutes DESC;`,
+        [req.params.id]
+      )
+      .then((data) => {
         res.json(data.rows);
       });
   });
