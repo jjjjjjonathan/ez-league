3a0e61c2080e31af0b32340d25953d6236b2b75a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-30 17:09:08 -0400

    Add route to create multiple new players

--- routes/players.js
+++ routes/players.js
@@ -10,12 +10,21 @@ module.exports = db => {
       });
   });
   router.put('/add', (req, res) => {
-    const { teamId, playerName, shirtNumber } = req.body
+    const { teamId, playerName, shirtNumber } = req.body;
     return db.query(`INSERT into players (team_id, name, shirt_number) VALUES($1,$2,$3) RETURNING *;`, [teamId, playerName, shirtNumber])
       .then(data => {
         res.status(201).json(data);
       });
-  })
+  });
+
+  router.put('/', (req, res) => {
+    const { queryString, queryParams } = req.body;
+    console.log(queryString, queryParams);
+    return db.query(queryString, queryParams)
+      .then(data => {
+        res.status(201).json(data);
+      });
+  });
   return router;
 };
 
